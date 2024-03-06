49977d90bbf3c13a97e270ea1f8910287dc8888a
Lewis M <2864939m@student.gla.ac.uk>
2024-03-06 16:45:36 +0000

    Chapter 4 tests complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
-    #return HttpResponse('<a href="/rango/about/">About</a> Rango says hey there partner!')
 
 
 def about(request):
-    return HttpResponse('<a href="/rango/">Index</a> Rango says here is the about page.')
+    return render(request, 'rango/about.html')
